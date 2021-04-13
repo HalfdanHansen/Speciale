7d271c3d7bc0ccef37af75526a1f40fc83b96d6d
Klara Emilie Elmegaard <elmegaardklara@gmail.com>
2021-04-13 14:03:50 +0200

    mnist conv500

--- TensorizedTrainingMethods/ApplyingCNN/MNIST/all_methods_on_mnist_conv500.py
+++ TensorizedTrainingMethods/ApplyingCNN/MNIST/all_methods_on_mnist_conv500.py
@@ -9,13 +9,16 @@ from MNIST_MODELS import *
 from train_val_test_MNIST import *
 from method_functions import *
 
+train_list = []
+test_list = []
+valid_list = []
 criterion = nn.CrossEntropyLoss()
 
 convName =  ["conv_1", "conv_2", "conv_3", "conv_4"]
 R = 1
 
 batch_size = 100
-num_epochs = 50
+num_epochs = 1
 num_samples_train = x_train.shape[0]
 num_batches_train = num_samples_train // batch_size
 num_samples_valid = x_valid.shape[0]
@@ -45,9 +48,9 @@ for epoch in range(num_epochs):
     train_acc.append(train_acc_cur)
     valid_acc.append(valid_acc_cur)
 
-    if epoch % 10 == 0:
-        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
-                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
+#    if epoch % 10 == 0:
+#        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
+#                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
 '''
 epoch = np.arange(len(train_acc))
 plt.figure()
@@ -59,7 +62,11 @@ plt.ylabel('Acc')
 preds, test_acc_full = evaluate_test(x_test, targets_test, netNormal)
 normalvalid_acc = valid_acc
 normaltest_acc = accuracy_score(list(targets_test), list(preds.data.numpy()))
-print("\nTest set Acc:  %f" % normaltest_acc)
+#print("\nTest set Acc:  %f" % normaltest_acc)
+
+train_list.append(train_acc)
+valid_list.append(normalvalid_acc)
+test_list.append(normaltest_acc)
 
 """Direct 4D Decompose Method
 
@@ -93,9 +100,9 @@ for epoch in range(num_epochs):
     train_acc.append(train_acc_cur)
     valid_acc.append(valid_acc_cur)
     
-    if epoch % 10 == 0:
-        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
-                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
+#    if epoch % 10 == 0:
+#        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
+#                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
 '''
 epoch = np.arange(len(train_acc))
 plt.figure()
@@ -107,37 +114,39 @@ plt.ylabel('Acc')
 preds, test_acc_full = evaluate_test(x_test, targets_test, netD4DD)
 D4DDvalid_acc = valid_acc
 D4DDtest_acc = (accuracy_score(list(targets_test), list(preds.data.numpy())))
-print("\nTest set Acc:  %f" % D4DDtest_acc)
+#print("\nTest set Acc:  %f" % D4DDtest_acc)
+
+train_list.append(train_acc)
+valid_list.append(D4DDvalid_acc)
+test_list.append(D4DDtest_acc)
 
 """Direct 3D Decompose Method"""
-
-from MNIST_CNN_conv500_3D import *
 
 train_acc, train_loss = [], []
 valid_acc, valid_loss = [], []
 test_acc, test_loss = [], []
 losses = []
 
-net3D.apply(weight_reset)
-optimizer3D = optim.Adam(net3D.parameters(), lr=0.001)
+convNet5003D.apply(weight_reset)
+optimizer3D = optim.Adam(convNet5003D.parameters(), lr=0.001)
 
 for epoch in range(num_epochs):
     cur_loss = 0
-    net3D.train()
-    losses = train_net(losses, num_batches_train, x_train, batch_size,targets_train, net3D, criterion, optimizer3D)
+    convNet5003D.train()
+    losses = train_net(losses, num_batches_train, x_train, batch_size,targets_train, convNet5003D, criterion, optimizer3D)
     train_preds, train_targs = [], []
 
-    train_targs, train_preds = evaluate_train(num_batches_train, x_train, batch_size, targets_train, net3D)
-    val_targs, val_preds = evaluate_validation(num_batches_valid, x_valid, batch_size, targets_valid, net3D)
+    train_targs, train_preds = evaluate_train(num_batches_train, x_train, batch_size, targets_train, convNet5003D)
+    val_targs, val_preds = evaluate_validation(num_batches_valid, x_valid, batch_size, targets_valid, convNet5003D)
 
     train_acc_cur = accuracy_score(train_targs, train_preds)
     valid_acc_cur = accuracy_score(val_targs, val_preds)
     train_acc.append(train_acc_cur)
     valid_acc.append(valid_acc_cur)
     
-    if epoch % 10 == 0:
-        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
-                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
+#    if epoch % 10 == 0:
+#        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
+#                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
 '''
 epoch = np.arange(len(train_acc))
 plt.figure()
@@ -146,19 +155,26 @@ plt.legend(['Train Acc', 'Val Acc'])
 plt.xlabel('Epochs')
 plt.ylabel('Acc')
 '''
-preds, test_acc_full = evaluate_test(x_test, targets_test, net3D)
+preds, test_acc_full = evaluate_test(x_test, targets_test, convNet5003D)
 D3DDvalid_acc = valid_acc
 D3DDtest_acc = (accuracy_score(list(targets_test), list(preds.data.numpy())))
-print("\nTest set Acc:  %f" % D3DDtest_acc)
+#print("\nTest set Acc:  %f" % D3DDtest_acc)
+
+train_list.append(train_acc)
+valid_list.append(D3DDvalid_acc)
+test_list.append(D3DDtest_acc)
 
 """ATDC Method 3D
 
 """
+netATDC = deepcopy(convNet500)
+netATDC.apply(weight_reset)
+optimizerATDC = optim.Adam(netATDC.parameters(), lr=0.001)
 
 pqt_convs = []
 
 for c in convName:
-  convData = eval("net."+c+".weight.data")
+  convData = eval("netATDC."+c+".weight.data")
   de_layer = []
   for cc in convData:
     temp = tl.decomposition.parafac(tl.tensor(cc), rank = 1)
@@ -169,10 +185,6 @@ train_acc, train_loss = [], []
 valid_acc, valid_loss = [], []
 test_acc, test_loss = [], []
 losses = []
-
-netATDC = deepcopy(convNet500)
-netATDC.apply(weight_reset)
-optimizerATDC = optim.Adam(netATDC.parameters(), lr=0.001)
 
 alpha = 0.1
 
@@ -185,9 +197,9 @@ for epoch in range(num_epochs):
     train_acc.append(train_acc_cur)
     valid_acc.append(valid_acc_cur)
     
-    if epoch % 10 == 0:
-        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
-                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
+#    if epoch % 10 == 0:
+#        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
+#                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
 '''
 epoch = np.arange(len(train_acc))
 plt.figure()
@@ -200,14 +212,22 @@ plt.ylabel('Acc')
 preds, test_acc_full = evaluate_test(x_test, targets_test, netATDC)
 ATDC3Dvalid_acc = valid_acc
 ATDC3Dtest_acc = (accuracy_score(list(targets_test), list(preds.data.numpy())))
-print("\nTest set Acc:  %f" % ATDC3Dtest_acc)
+#print("\nTest set Acc:  %f" % ATDC3Dtest_acc)
+
+train_list.append(train_acc)
+valid_list.append(ATDC3Dvalid_acc)
+test_list.append(ATDC3Dtest_acc)
 
 """ATDC method 4D"""
+
+netATDC4D = deepcopy(convNet500)
+netATDC4D.apply(weight_reset)
+optimizerATDC4D = optim.Adam(netATDC4D.parameters(), lr=0.001)
 
 pqtu_convs = []
 
 for c in convName:
-  convData = eval("net."+c+".weight.data")
+  convData = eval("netATDC4D."+c+".weight.data")
   temp = tl.decomposition.parafac(tl.tensor(convData), rank = 1)
   pqtu_convs.append(temp)
 
@@ -215,10 +235,6 @@ train_acc, train_loss = [], []
 valid_acc, valid_loss = [], []
 test_acc, test_loss = [], []
 losses = []
-
-netATDC4D = deepcopy(convNet500)
-netATDC4D.apply(weight_reset)
-optimizerATDC4D = optim.Adam(netATDC4D.parameters(), lr=0.001)
 
 alpha = 0.1
 # run training with ATDC step
@@ -236,9 +252,9 @@ for epoch in range(num_epochs):
     train_acc.append(train_acc_cur)
     valid_acc.append(valid_acc_cur)
     
-    if epoch % 5 == 0:
-        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
-                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
+#    if epoch % 5 == 0:
+#        print("Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
+#                epoch+1, losses[-1], train_acc_cur, valid_acc_cur))
 
 '''
 epoch = np.arange(len(train_acc))
@@ -252,8 +268,17 @@ plt.ylabel('Acc')
 preds, test_acc_full = evaluate_test(x_test, targets_test, netATDC4D)
 ATDC4Dvalid_acc = valid_acc
 ATDC4Dtest_acc = (accuracy_score(list(targets_test), list(preds.data.numpy())))
-print("\nTest set Acc:  %f" % ATDC4Dtest_acc)
+#print("\nTest set Acc:  %f" % ATDC4Dtest_acc)
 
+train_list.append(train_acc)
+valid_list.append(ATDC4Dvalid_acc)
+test_list.append(ATDC4Dtest_acc)
+
+save_train = pd.DataFrame(train_list)
+save_valid = pd.DataFrame(valid_list)
+save_test = pd.DataFrame(test_list)
+
+pd.concat([save_train,save_valid,save_test], axis = 0).to_csv('MNIST_5methods.csv',index=False,header=False)
 
 '''
 """Comparing the Methods"""
@@ -332,5 +357,3 @@ ATDC3Dtest_acc = 0.952
 ATDC4Dtest_acc = 0.92
 
 '''
-save_train = pd.DataFrame()
-to_csv()
