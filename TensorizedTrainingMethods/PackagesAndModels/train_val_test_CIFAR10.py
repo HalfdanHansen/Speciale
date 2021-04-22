5da185dd9b1d01a7a6c71261fac447ccf48b9fbf
Halfdan Lasse Runge Hansen <nadflah1@gmail.com>
2021-04-22 09:21:01 +0200

    train CP 3d added

--- TensorizedTrainingMethods/PackagesAndModels/train_val_test_CIFAR10.py
+++ TensorizedTrainingMethods/PackagesAndModels/train_val_test_CIFAR10.py
@@ -103,4 +103,37 @@ def train_net_PARAFAC4D_ATDC(losses, net, netname, trainloader, criterion, optim
   return running_loss
 
 
-  ## make train net parafac3d ATDC
+def train_net_PARAFAC3D_ATDC(losses, net, netname, trainloader, criterion, optimizer, convName, pqt_convs, alpha, rank, lName):
+  running_loss = 0
+  net.train()
+
+  for i, data in enumerate(trainloader, 0):
+    inputs, labels = data[0].cuda(), data[1].cuda()
+    
+    optimizer.zero_grad()
+    outputs = net(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    #optimizer.step()
+
+    #ATDC step for convolutional layers
+    for k1,pqt in enumerate(pqt_convs):
+
+      convGrad = eval(netname+"."+convName[k1]+".weight.grad")
+      convData = eval(netname+"."+convName[k1]+".weight.data")
+      for k2,pqt_filter in enumerate(pqt):
+        pqt_convs[k1][k2] = ATDC_get_grads_one_filter_3D_short_tensor(
+                            ATDC_update_step_one_filter_3D_tensor(convGrad, pqt_filter), alpha, pqt_filter)
+
+        convData[k2] = torch.einsum('s,i,j->sij',pqt_filter[0],pqt_filter[1],pqt_filter[2])
+
+      #Magi # its seems like you need a if '__name__' == __main__ guard! #einsum -> winsum
+
+    #normal step for linear layer
+    for name in lName:
+      eval('net.'+name+'.weight.data[:] = torch.sub(net.'+name+'.weight.data,net.'+name+'.weight.grad, alpha = alpha)')
+      eval('net.'+name+'.bias.data[:] = torch.sub(net.'+name+'.bias.data,net.'+name+'.bias.grad,alpha = alpha)')
+      
+    running_loss += loss.item()
+
+  return running_loss
